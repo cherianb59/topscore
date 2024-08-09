9eed559f81b65eb761b634b162ed04ec0785236f
Ben Cherian <cherian.b59@gmail.com>
2024-08-09 16:17:01 +1000

    add pypi details

--- setup.py
+++ setup.py
@@ -9,14 +9,8 @@ setup(
     version='0.0.1',
     packages=['topscore'],
     url='https://github.com/cherianb59/topscore',
-    license='',
     author='Ben Cherian, Ian Marlier',
     author_email='',
-    keywords="topscore usetopscore ultimate frisbee",
     description='Python API Client for TopScore',
-    install_requires=[
-    	'requests',
-        'aiohttp',
-        'beautifulsoup4'
-    	]
+    
 )
