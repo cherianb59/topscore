21cb8d776a17952dfafe01aae708d63fa651bc2a
Ben Cherian <cherian.b59@gmail.com>
2024-08-14 09:51:56 +1000

    add initial typing

--- topscore/client.py
+++ topscore/client.py
@@ -5,11 +5,13 @@ import time
 import uuid
 import re 
 import asyncio
+from typing import List, Dict, Optional, Union
 
 import requests
 import aiohttp
 from bs4 import BeautifulSoup
 import urllib.parse
+
 
 #CONSTS
 
@@ -20,19 +22,21 @@ default_headers = {
 
 #HELPERS 
 
-def get_only_endpoint(url):
-    parsed_url = urllib.parse.urlparse(url)
-    path_and_query = parsed_url.path + "?" + parsed_url.query
-    decoded_path_and_query = urllib.parse.unquote(path_and_query)
-    #get rid of '/api'
-    return decoded_path_and_query[4:]
+def get_only_endpoint(url: str) -> str:
+  #Get the bit after /api in an api call
+  #Used for getting all help pages
+  parsed_url = urllib.parse.urlparse(url)
+  path_and_query = parsed_url.path + "?" + parsed_url.query
+  decoded_path_and_query = urllib.parse.unquote(path_and_query)
+  #get rid of '/api'
+  return decoded_path_and_query[4:]
 
 
-class TopScoreException(Exception):
+class TopScoreException(Exception) :
   pass
           
 class TopScoreClient(object):
-  def __init__(self,  base_url, oauth_client_id = '' , oauth_client_secret = '' , email = '' , password = '' , headers = default_headers):
+  def __init__(self,  base_url: str, oauth_client_id: Optional[str] = '' , oauth_client_secret: Optional[str] = '' , email: Optional[str] = '' , password: Optional[str] = '' , headers: Optional[dict] = default_headers):
     
     if not base_url : raise TopScoreException(f"""base_url required""")
     
@@ -62,7 +66,7 @@ class TopScoreClient(object):
     self.access_token = self.get_oauth_access_token()
 
   #submit oauth creds and then get the acces token   
-  def get_oauth_access_token(self):
+  def get_oauth_access_token(self) -> str:
     url = f"{self.base_url}/api/oauth/server"
      
     oauth_params= {'grant_type':'client_credentials',
@@ -79,7 +83,7 @@ class TopScoreClient(object):
       raise TopScoreException(f"OAuth Fail\n{result}")
 
   # login to the site, Scrape the oauth creds 
-  def extract_oauth_tokens(self):
+  def extract_oauth_tokens(self) -> Dict:
 
     # Replace "ultimatecentral" with "usetopscore"
     self.base_url = re.sub(r"(?<=\.)ultimatecentral(?=\.com)", "usetopscore", self.base_url)
@@ -145,7 +149,7 @@ class TopScoreClient(object):
 
   #Client Side Request Forgery token generation, make a signature based on the client secret without transmitting secret.
   #function currently unused
-  def csrf(self):
+  def csrf(self) -> str:
     
     nonce = bytes(str(uuid.uuid4()), 'ascii')
     ts = bytes(str(round(time.time())), 'ascii')
@@ -154,11 +158,12 @@ class TopScoreClient(object):
     h = hmac.new(secret, message, digestmod=hashlib.sha256)
     return base64.urlsafe_b64encode(nonce + b'|' + ts + b'|' + base64.urlsafe_b64encode(h.digest()))
 
-  def construct_url(self, endpoint):
+  def construct_url(self, endpoint: str) -> str:
     return f"{self.base_url}/api/{endpoint}"
 
   # base function for getting data
-  def get(self, endpoint, page=1, per_page=100, auth_fail = False,  **params):
+  def get(self, endpoint: str, page: Optional[int] =1, per_page: Optional[int] =100, auth_fail: Optional[bool]  = False,  **params):
+    
     params['page'] = page
     params['per_page'] = per_page
     headers = {"Authorization": f"Bearer {self.access_token}"} | self.headers
@@ -169,15 +174,16 @@ class TopScoreClient(object):
 
     #if access_token fails then regenerate and retry
     if rjson['status'] == 401 and rjson['errors'][0]['message'] == "Invalid auth token." :      
-    if not auth_fail:
-      self.access_token = self.get_oauth_access_token()
-      return(self.get(endpoint, page, per_page, auth_fail = True,  **params))
+      if not auth_fail:
+        self.access_token = self.get_oauth_access_token()
+        return(self.get(endpoint, page, per_page, auth_fail = True,  **params))
     else : 
       raise TopScoreException(f"Auth token failed after retry")
   
     return(r)
 
-  def post(self, endpoint, data={}, page=1, per_page=100, **params):
+  def post(self, endpoint, data: Optional[Dict]={}, page: Optional[int] =1, per_page: Optional[int] =100, **params: Optional[Dict]):
+  
     params['page'] = page
     params['per_page'] = per_page
     headers = {"Authorization": f"Bearer {self.access_token}"} | self.headers
@@ -188,7 +194,7 @@ class TopScoreClient(object):
     r = self.post("me")
     return r.json()
 
-  def update_game(self, game_id, field, value, **params):
+  def update_game(self, game_id: Union[int,str], field: Dict, value, **params: Dict):
     r = self.post("games/edit", data={
       'game_ids': [game_id],
       'field': field,
@@ -197,7 +203,7 @@ class TopScoreClient(object):
     return r.json()
 
   #creator_id and end are required
-  def update_event(self, event_id, creator_id, end,  **params):
+  def update_event(self, event_id: Union[int,str], creator_id: Union[int,str], end: str,  **params: Optional[Dict]):
     r = self.post("events/edit", data={
       'id': [event_id],
       creator_id : creator_id ,
@@ -205,13 +211,12 @@ class TopScoreClient(object):
     }, **params)
     return r.json()
 
-  #TODO write functions to parse and display results of help pages
   def get_help(self):
     r = self.get("help")
     return r.json()
 
   # async function to make a single request
-  async def fetch(self, endpoint, session, page=1, per_page=100,  **params ):
+  async def fetch(self, endpoint: str, session, page: Optional[int] =1, per_page: Optional[int] =100,  **params ) ->List[Dict] :
     
     params['page'] = page
     params['per_page'] = per_page
@@ -228,7 +233,7 @@ class TopScoreClient(object):
 
   #async function to make multiple requests to get results from results with over 100 entries
   #Usually not called directly
-  async def fetch_all(self, endpoint, page=1, per_page=100, **params):
+  async def fetch_all(self, endpoint: str, page: Optional[int] =1, per_page: Optional[int] =100, **params) -> List[Dict]:
     
     result = self.get(endpoint, page, per_page, **params).json()
     results = result['result']
@@ -248,7 +253,7 @@ class TopScoreClient(object):
       raise TopScoreException(f"Endpoint: {endpoint} \n Params: {params} \n Result:{result} \nUnable to get a paginated response from TopScore")
   
   #Use this function to get all results from GET endpoints  
-  def get_all_pages(self,endpoint, **params):
+  def get_all_pages(self,endpoint: str, **params) -> List[Dict]:
     endpoints = ["tags","games","registrations","events","persons","teams","fields","transactions","persons"]
     
     if endpoint in endpoints :
@@ -257,7 +262,7 @@ class TopScoreClient(object):
     else: raise TopScoreException(f"{endpoint} not in {endpoints}")
 
   # non async functions to get data  
-  def get_paginated(self, endpoint, page=1, per_page=100, **params):
+  def get_paginated(self, endpoint:str , page: Optional[int] =1, per_page: Optional[int] =100, **params) -> List[Dict]:
     result = self.get(endpoint, page, per_page, **params).json()
     results = result['result']
 
@@ -282,38 +287,38 @@ class TopScoreClient(object):
     r = self.get("games/show", **params)
     return r.json()
     
-  def get_tags(self, **params):
+  def get_tags(self, **params)->List[Dict]:
     return self.get_paginated("tags", **params)
 
-  def get_games(self, **params):
+  def get_games(self, **params)->List[Dict]:
     return self.get_paginated("games", **params)
 
-  def get_registrations(self, **params):
+  def get_registrations(self, **params)->List[Dict]:
     return self.get_paginated("registrations", **params)
 
-  def get_events(self, **params):
+  def get_events(self, **params)->List[Dict]:
     return self.get_paginated("events", **params)
 
-  def get_people(self, **params):
+  def get_people(self, **params)->List[Dict]:
     return self.get_paginated("persons", **params)
 
-  def get_persons(self, id, **params):
+  def get_persons(self, id, **params)->List[Dict]:
     return self.get_paginated("persons", **params)
 
-  def get_teams(self, **params):
+  def get_teams(self, **params)->List[Dict]:
     return self.get_paginated("teams", **params)
 
-  def get_fields(self, **params):
+  def get_fields(self, **params)->List[Dict]:
     return self.get_paginated("fields", **params)
 
-  def get_transactions(self, **params):
+  def get_transactions(self, **params)->List[Dict]:
     return self.get_paginated("transactions", **params)
 
-  def get_persons(self,  **params):
+  def get_persons(self,  **params) ->List[Dict]:
     return self.get_paginated("persons", **params)
 
   #get the help page and add all relevant fields
-  def get_all_help(self):
+  def get_all_help(self)-> List[Dict]:
     get_help = self.get_help()
     
     get_help = { h['endpoint']: h for h in get_help['result'] if 'endpoint' in h }
@@ -322,11 +327,11 @@ class TopScoreClient(object):
     for k,v in get_help.items():
       v['GET'] = 'GET' in v['method']
       v['POST'] = 'POST' in v['method']
-
-    self.help_docs = fetch_all_help(get_help)
+    #add all the fields for each endpoint
+    return( fetch_all_help(get_help))
     
+  async def fetch_all_help(self, help_endpoints: List[Dict], **params) -> List[Dict]:
   #fetch the docs for all endpoints simultaneously  
-  async def fetch_all_help(self, help_endpoints, **params):
     async with aiohttp.ClientSession() as session:
       
       tasks = [client.fetch(get_only_endpoint(v['help_url']) , session , page = 1,  per_page = 100 , **params) for k,v in help_endpoints.items() ]
